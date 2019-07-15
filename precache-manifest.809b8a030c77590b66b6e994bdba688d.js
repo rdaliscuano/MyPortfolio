1bc4ab32038ce7a60b1a4ff7404a589a778ef6ac
rdaliscuano <Roldanaliscuano@gmail.com>
2019-07-15 16:38:09 +0800

    Updates

---
+++ precache-manifest.809b8a030c77590b66b6e994bdba688d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301101b12e7cb83a515610d81a5bf309",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "e56ac592cb65ea318acf",
+    "url": "/MyPortfolio/static/css/main.33f557d4.chunk.css"
+  },
+  {
+    "revision": "c4c02382ef9e5bf1f3aa",
+    "url": "/MyPortfolio/static/js/2.1faff79c.chunk.js"
+  },
+  {
+    "revision": "e56ac592cb65ea318acf",
+    "url": "/MyPortfolio/static/js/main.8b8f3186.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "d86d6e5d91370b8828147aa14e942076",
+    "url": "/MyPortfolio/static/media/CV.d86d6e5d.png"
+  },
+  {
+    "revision": "e0f86d52cf6cc01e24efcd65f4ef4e68",
+    "url": "/MyPortfolio/static/media/council.e0f86d52.jpg"
+  },
+  {
+    "revision": "d9f15064a3276fcce964fce46b10c851",
+    "url": "/MyPortfolio/static/media/lpu.d9f15064.jpg"
+  }
+]);
