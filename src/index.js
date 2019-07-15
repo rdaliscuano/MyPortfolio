661960fdee4db023466502104dd612ecb25a08fa
rdaliscuano <Roldanaliscuano@gmail.com>
2019-07-15 17:46:46 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {HashRouter as Router} from 'react-router-dom';
-
+import Navbar from './Components/Navbar';
 
 
 
 ReactDOM.render(
 <Router >
-    <App/>
+    <Navbar/>
 </Router>
 ,document.getElementById('root'));
 
